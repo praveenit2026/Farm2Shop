2a5994a39fe00026f2b8ccccd9ce07a948f116f0
praveenit2026 <praveenwork0004@gmail.com>
2026-07-27 14:45:35 +0530

    Fix: disable Tomcat shutdown port to stop Render health check warnings

--- start.sh
+++ start.sh
@@ -4,8 +4,11 @@ PORT=${PORT:-10000}
 
 echo "Starting Tomcat on port $PORT"
 
-# Replace the Connector port in server.xml with Render's PORT
+# Replace the HTTP Connector port with Render's PORT
 sed -i "s/port=\"8080\"/port=\"$PORT\"/g" /usr/local/tomcat/conf/server.xml
+
+# Disable Tomcat shutdown port (set to -1) to avoid Render health check warnings
+sed -i 's/<Server port="8005" shutdown="SHUTDOWN">/<Server port="-1" shutdown="SHUTDOWN">/' /usr/local/tomcat/conf/server.xml
 
 # Start Tomcat
 exec /usr/local/tomcat/bin/catalina.sh run
